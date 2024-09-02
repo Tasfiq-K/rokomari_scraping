737b9ddb26400334b27815a0daef6aedc8badf9d
tasfiq-k <tasfiq.kamran@gmail.com>
2024-09-02 15:59:41 +0600

    added code for parse and parse publishers

--- rokomariScraper/rokomariScraper/spiders/rokomariBooks.py
+++ rokomariScraper/rokomariScraper/spiders/rokomariBooks.py
@@ -7,10 +7,28 @@ class RokomaribooksSpider(scrapy.Spider):
     start_urls = ["https://www.rokomari.com/book/publishers?ref=sm_p2"]
 
     def parse(self, response):
-        pass
+        links = response.css(".authorList a::attr(href)").getall()
+
+        if links:
+            for link in links:
+                yield response.follow(link, callback=self.parse_publishers)
+
+        next_page = response.css("a:nth-child(13)::attr(href)").get()
+
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
 
     def parse_publishers(self, response):
-        pass
+        book_links = response.css(".book-list-wrapper a::attr(href)").getall()
 
-    def parse_info(self, response):
+        if book_links:
+            for book_link in book_links:
+                yield response.follow(book_link, callback=self.parse_books)
+
+        next_page = response.css("a:nth-child(7)::attr(href)").get()
+
+        if next_page:
+            yield response.follow(next_page, callback=self.parse_publishers)
+
+    def parse_books(self, response):
         pass
